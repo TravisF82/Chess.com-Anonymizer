ff89f11f04c817f48c0eb31c340b83584884b4e9
TravisF82 <travisdevG82@gmail.com>
2026-06-16 21:04:30 +0100

    Hiding avatar, username, rating and flag

--- content.js
+++ content.js
@@ -1,19 +1,42 @@
 async function applySettings(){
     const settings = await chrome.storage.sync.get([
-        "hideUsername"
+        "hideUsername",
+        "hideRating",
+        "hideAvatar",
+        "hideFlag",
+        "hideMembership",
     ]);
-
+    
     document.querySelector(".cc-user-username-component")
         ?.style.setProperty(
             "display",
             settings.hideUsername ? "none" : ""
         );
+        
+    document.querySelector(".cc-user-rating-white")
+        ?.style.setProperty(
+            "display",
+            settings.hideRating ? "none" : ""
+        );
 
+    document.querySelector(".player-row-avatar")
+        ?.style.setProperty(
+            "display",
+            settings.hideAvatar ? "none" : ""
+        );
+
+    document.querySelector(".cc-country-flag-component")
+        ?.style.setProperty(
+            "display",
+            settings.hideFlag ? "none" : ""
+        );
 
 }
 
 chrome.runtime.onMessage.addListener(async (message) => {
-    
+    if (message.action === "settingChanged"){
+        applySettings();
+    }
 });
 
 const observer = new MutationObserver(() => {
--- index.js
+++ index.js
@@ -4,7 +4,21 @@ let chkName = document.getElementById("hide-name");
 let chkFlag = document.getElementById("hide-flag");
 let chkMembership = document.getElementById("hide-membership");
 
-const hide = "hidden";
+document.addEventListener("DOMContentLoaded", async () => {
+    const settings = await chrome.storage.sync.get([
+        "hideUsername",
+        "hideRating",
+        "hideAvatar",
+        "hideFlag",
+        "hideMembership",
+    ]);
+
+    chkName.checked = settings.hideUsername ?? false;
+    chkRating.checked = settings.hideRating ?? false;
+    chkAvatar.checked = settings.hideAvatar ?? false;
+    chkFlag.checked = settings.hideFlag ?? false;
+    chkMembership.checked = settings.hideMembership ?? false;
+});
 
 chkName.addEventListener("change", async () => {
     await chrome.storage.sync.set({
@@ -17,7 +31,67 @@ chkName.addEventListener("change", async () => {
         });
 
     chrome.tabs.sendMessage(tab.id, {
-            action: "toggleUsername"
+            action: "settingChanged"
+        });
+});
+
+chkRating.addEventListener("change", async () => {
+    await chrome.storage.sync.set({
+            hideRating: chkRating.checked
+        });
+
+    const [tab] = await chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        });
+
+    chrome.tabs.sendMessage(tab.id, {
+            action: "settingChanged"
+        });
+});
+
+chkAvatar.addEventListener("change", async () => {
+    await chrome.storage.sync.set({
+            hideAvatar: chkAvatar.checked
+        });
+
+    const [tab] = await chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        });
+
+    chrome.tabs.sendMessage(tab.id, {
+            action: "settingChanged"
+        });
+});
+
+chkFlag.addEventListener("change", async () => {
+    await chrome.storage.sync.set({
+            hideFlag: chkFlag.checked
+        });
+
+    const [tab] = await chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        });
+
+    chrome.tabs.sendMessage(tab.id, {
+            action: "settingChanged"
+        });
+});
+
+chkMembership.addEventListener("change", async () => {
+    await chrome.storage.sync.set({
+            hideMembership: chkMembership.checked
+        });
+
+    const [tab] = await chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        });
+
+    chrome.tabs.sendMessage(tab.id, {
+            action: "settingChanged"
         });
 });
 
